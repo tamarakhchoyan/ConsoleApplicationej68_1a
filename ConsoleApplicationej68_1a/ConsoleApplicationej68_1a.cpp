ddcdaa92ff12bf1f29bf59a3b92a0994557e96fa
User <User@DESKTOP-9TGTEUB>
2023-12-10 15:12:10 +0400

    Add project files.

---
+++ ConsoleApplicationej68_1a/ConsoleApplicationej68_1a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main()
+{
+	const int n = 8;
+	int i, a[n], count=0;
+	for (i = 0; i < n; ++i)
+		std::cin >> a[i];
+	for (i = 0; i < n; ++i) {
+		if(a[i]>=-5 && a[i]<=10)
+		{
+			count++;
+			std::cout << count;
+		}
+	}
+	return 0;
+}
+
